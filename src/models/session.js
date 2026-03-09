489d49306d577da86a71f368ef0ceb38b7b189c7
Oleksandr Maliuk <maliukolexandr@gmail.com>
2026-03-09 21:11:58 +0200

    feat: make refreshTokenHash unique in session schema

--- src/models/session.js
+++ src/models/session.js
@@ -4,7 +4,7 @@ const sessionSchema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     accessToken: { type: String, required: true, unique: true },
-    refreshTokenHash: { type: String, required: true },
+    refreshTokenHash: { type: String, required: true, unique: true },
     rotationCount: { type: Number, default: 0 },
     revoked: { type: Boolean, default: false },
     ip: String,
@@ -18,11 +18,7 @@ const sessionSchema = new Schema(
     versionKey: false,
   },
 );
-
-sessionSchema.index({ accessToken: 1 }, { unique: true });
-sessionSchema.index({ refreshTokenHash: 1 }, { unique: true });
 sessionSchema.index({ userId: 1 });
-
 sessionSchema.index({ refreshTokenValidUntil: 1 }, { expireAfterSeconds: 0 });
 
 export const Session = model('Session', sessionSchema);
