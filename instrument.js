794e3d6bfcd87ca86c99b784aa2025b0eebd9451
Oleksandr Maliuk <maliukolexandr@gmail.com>
2026-05-04 23:45:52 +0300

    feat: add Sentry profiling integration for enhanced performance monitoring

--- instrument.js
+++ instrument.js
@@ -1,9 +1,15 @@
 // Import with `import * as Sentry from "@sentry/node"` if you are using ESM
 // const Sentry = require('@sentry/node');
 import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN, // DSN з Sentry
-  sendDefaultPii: true, // Дозволяє бачити IP користувачів (корисно для відстеження атак)
-  tracesSampleRate: 1.0, // Фіксувати 100% запитів для моніторингу продуктивності
+  integrations: [
+    nodeProfilingIntegration(), // Включає детальний аналіз швидкості функцій
+  ],
+  enableLogs: true, // Виводитиме логи ініціалізації в консоль (корисно для дебагу)
+  tracesSampleRate: 1.0, // Записувати 100% транзакцій (запитів)
+  profilesSampleRate: 1.0, // Аналізувати 100% сесій на предмет продуктивності
+  sendDefaultPii: true, // Збирати IP та іншу мета-інформацію
 });
